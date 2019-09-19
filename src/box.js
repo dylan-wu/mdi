019e42f7ee8b9eda3071d59025c0a5593ee87776
Gary Wu <garywu@live.com>
2019-09-19 15:01:50 -0500

    fix #1: add progress for box

--- src/box.js
+++ src/box.js
@@ -1,23 +1,46 @@
 
-function body(params){ 
+function body(progress, scale, params){ 
     if ('c' in params){
         console.log('k')
         if (!('color' in params)){
             params.color = params.c
         }
     }
-    console.log(params)
+    
+    const ratio = progress/scale
+
+    if (!('color' in params) && progress){
+        params.color = "#d9534f"
+        if (ratio > 0.3){
+            if (ratio > 0.7) {
+                params.color =  "#5cb85c"
+            } else {
+                params.color = "#f0ad4e"
+            }
+        }
+    }
+    let text = ''
+    if (progress){
+        text =`<g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="14">
+            <text x="50%" y="50%" text-anchor="middle">
+            ${params.title?(programs.title + ' '):'' + ratio * 100}%
+            </text>
+        </g>`
+    }
+
     return `<?xml version="1.0" encoding="UTF-8"?>
-        <svg width="100%" height="100%" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="xMidYMid">
-            <rect width="100%" height="100%" fill="${params.color}" />
+        <svg width="120" height="60" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="xMidYMid">
+            <rect width="120" height="60" fill="${params.color}" />
+            ${text}
         </svg>`
 }
 
 exports.handler = function(event, context, callback) {
     console.log(event)
+    const progress = parseInt(event.path.substring(event.path.lastIndexOf('/') + 1))
     callback(null, {
         statusCode: 200,
         headers: {"Content-Type":"image/svg+xml"},
-        body: body(event.queryStringParameters)
+        body: body(progress, 100, event.queryStringParameters)
         });
 }
