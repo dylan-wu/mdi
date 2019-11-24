e717261771046350d780713aa936d632f1a1f04e
= <=>
2019-11-24 13:56:35 -0600

    fix view

--- src/bar.js
+++ src/bar.js
@@ -11,10 +11,12 @@ function body(progress, scale, params={}){
     }
 
     title = ''
-    progressX = 0
+    progressX = 54
+    width = 60
 
     if ('title' in params){
         progressX = 94.0
+        width = ratio * 60
         title = `<rect rx="4" width="124" height="20" fill="url(#a)" />
         <g fill="#fff" text-anchor="left" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="11">
         <text x="4" y="15" fill="#010101" fill-opacity=".3">
@@ -35,7 +37,7 @@ function body(progress, scale, params={}){
 
             <rect rx="4" x="0" width="124" height="20" fill="#428bca"/>
             <rect rx="4" x="64" width="60" height="20" fill="#555" />
-            <rect rx="4" x="64" width="${ratio * 60}" height="20" fill="${color}" />
+            <rect rx="4" x="64" width="${width}" height="20" fill="${color}" />
                 <path fill="${color}" d="M64 0h4v20h-4z" />${title}
             <g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="11">
                 <text x="${progressX}" y="15" fill="#010101" fill-opacity=".3">
