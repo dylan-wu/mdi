3de95c91b292638d4d10a3742ec968f560ba5d64
Dylan Wu <dylan-wu@users.noreply.github.com>
2019-12-23 15:52:14 -0600

    Update box.js

--- src/box.js
+++ src/box.js
@@ -20,7 +20,7 @@ function body(progress, scale, params){
 
     let text = ''
     if (progress || params.title){
-        text =`<g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="14">
+        text =`<g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="12">
             <text x="50%" y="52%" text-anchor="middle">
             ${(params.title?(params.title + ' '):'') + (progress?(Math.round(ratio * 100) + '%'):'')}
             </text>
@@ -28,7 +28,7 @@ function body(progress, scale, params){
     }
 
     return `<?xml version="1.0" encoding="UTF-8"?>
-        <svg width="120" height="60" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="xMidYMid">
+        <svg width="80" height="40" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="xMidYMid">
             <rect width="80" height="40" fill="${params.color}" />
             ${text}
         </svg>`
